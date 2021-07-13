6e0c6c6d84d3a30decbe7d114686935349e712fa
mrlouislow <mrlouislow@gmail.com>
2021-07-14 00:30:53 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -17,6 +17,8 @@ provider "aws" {
 resource "aws_instance" "app_server" {
   ami           = "ami-830c94e3"
   instance_type = "t2.micro"
+  access_key = var.awsaccess
+  secret_key = var.awssecret
 
   tags = {
     Name = "ExampleAppServerInstance"
